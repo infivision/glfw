bd6121866ff7f1c4d2623594c7847feaa685ea0c
guanmingtao <guanmingtao@infivision.com.cn>
2021-01-17 10:10:54 +0800

    fix bugs on x86 arch

--- build_osx_x86.sh
+++ build_osx_x86.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+#!/bin/bash
+rm -rf build/
+INFI_OS=osx
+INFI_ARCH=x86
+
+source ./build_project_inc.sh
+
 mkdir -p build/
 cd build
-/Applications/CMake.app/Contents/bin/cmake -DCMAKE_CXX_COMPILER="c++" -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++" -DCMAKE_OSX_ARCHITECTURES="x86_64" ../
+/Applications/CMake.app/Contents/bin/cmake -DCMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR ../
 make
+make install
 cd ..
 
-mkdir -p ../infistd/lib/osx/x86/
-cp -v build/lib/libgtest.a ../infistd/lib/osx/x86/
